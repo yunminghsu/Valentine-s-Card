f78c3c6315db6fc9657ba2a6c36476eef87515ae
Arthur Ming <howard5816632@gmail.com>
2022-08-05 16:18:29 +0800

    add some comments

--- Contents.swift
+++ Contents.swift
@@ -9,6 +9,7 @@ catImage.layer.borderColor = CGColor(red: 1, green: 181/255, blue: 197/255, alph
 catImage.layer.borderWidth=5
 catImage.clipsToBounds=true
 
+// create I Love You stick
 let messageLabel = UILabel(frame: CGRect(x: 150, y: 190, width: 130, height: 20))
 messageLabel.text="  I  Love You "
 messageLabel.textColor=UIColor(red: 255/255, green: 20/255, blue: 147/255, alpha: 1)
@@ -16,6 +17,7 @@ messageLabel.font=UIFont.systemFont(ofSize: 25)
 // messageLabel.font=messageLabel.font.withSize(36)
 catImage.addSubview(messageLabel)
 
+// loop to create the cat border
 for i in 0...10{
     let starLabel=UILabel(frame: CGRect(x: 5+i*30, y: 15, width: 20, height: 10))
     let starLabel2=UILabel(frame: CGRect(x: 5+i*30, y: 225, width: 20, height: 10))
